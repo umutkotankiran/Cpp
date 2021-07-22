49bff512aeea535010335a64c64eff5c76549200
cdr1917 <79421649+cdr1917@users.noreply.github.com>
2021-07-22 11:31:46 +0300

    Add files via upload

---
+++ 07_23_05_2021/date.cpp
@@ -0,0 +1,21 @@
+﻿#include "date.h"
+
+void Date::set(int d, int m, int y)
+{
+	m = mm;
+	d = md;
+	y = my;
+}
+
+/*
+ 
+EĞER AŞAĞIDAKİ GİBİ TANIMLANIRSA GLOBAL FUNCTION OLUR
+Derleyici class member function olan set function olduğunu yukarıdaki gibi anlar.
+Aşağıdaki gibi olursa GLOBAL FUNCTION olur.
+
+void set(int d, int m, int y)
+{
+
+}
+
+*/
---
+++ 07_23_05_2021/date.h
@@ -0,0 +1,16 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date {
+public:
+	void set(int d, int m, int y); //void in 3 parametresi yok. 1 tanede gizli parametresi var.
+									//	4 parametresi var.
+private:
+	int mm, md, my;
+};
+
+
+
+
+
+#endif
